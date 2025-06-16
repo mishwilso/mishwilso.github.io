9ee0ced46bff24b754e3414194d51dd6aa71df3e
Mish Wilson <mwilso33@uvm.edu>
2025-06-16 14:03:08 -0400

    build

--- public/legacy-site/reactMount.js
+++
@@ -1,5 +0,0 @@
-// File: reactMount.js
-window.mountReactApp = function (targetEl, ComponentFn) {
-  const root = ReactDOM.createRoot(targetEl);
-  root.render(React.createElement(ComponentFn));
-};
